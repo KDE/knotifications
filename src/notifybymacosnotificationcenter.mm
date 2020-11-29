a54edfe78f2227ed010128d8bfac80c5418c0aac
Weixuan XIAO <veyx.shaw@gmail.com>
2020-11-29 21:04:11 +0100

    Add SPDX license statement for the impl on macOS

--- src/notifybymacosnotificationcenter.mm
+++ src/notifybymacosnotificationcenter.mm
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2019-2020 Weixuan XIAO <veyx.shaw at gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
 #include "notifybymacosnotificationcenter.h"
 #include "knotification.h"
 #include "knotifyconfig.h"

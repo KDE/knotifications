53a9b159ab9068fec19a9fbeec28e123d40c2fb2
Nicolas Fella <nicolas.fella@gmx.de>
2024-03-29 13:26:24 +0100

    Port QML module to declarative type registration

--- src/qml/knotificationqmlplugin.cpp
+++ src/qml/knotificationqmlplugin.cpp
@@ -6,110 +6,64 @@
 
 #include "knotificationqmlplugin.h"
 
-#include <KNotification>
-#include <KNotificationPermission>
-#include <KNotificationReplyAction>
-
-class NotificationWrapper : public KNotification
+NotificationWrapper::NotificationWrapper(QObject *parent)
+    : KNotification(QString(), KNotification::CloseOnTimeout, parent)
 {
-    Q_OBJECT
-    Q_PROPERTY(KNotificationReplyAction *replyAction READ replyActionFactory CONSTANT)
-    Q_PROPERTY(QQmlListProperty<KNotificationAction> actions READ actionsProperty NOTIFY actionsChanged)
-    Q_PROPERTY(KNotificationAction *defaultAction READ defaultAction WRITE setDefaultActionQml NOTIFY defaultActionChanged)
-public:
-    explicit NotificationWrapper(QObject *parent = nullptr)
-        : KNotification(QString(), KNotification::CloseOnTimeout, parent)
-    {
-        setAutoDelete(false);
+    setAutoDelete(false);
 
-        m_actionsProperty = QQmlListProperty<KNotificationAction>(this,
-                                                                  nullptr,
-                                                                  &NotificationWrapper::appendAction,
-                                                                  &NotificationWrapper::actionsCount,
-                                                                  &NotificationWrapper::actionAt,
-                                                                  &NotificationWrapper::clearActions);
-    }
-
-    KNotificationReplyAction *replyActionFactory()
-    {
-        if (!replyAction()) {
-            setReplyAction(std::make_unique<KNotificationReplyAction>(QString()));
-        }
-        return replyAction();
-    }
-
-    int actionCount() const
-    {
-        return actions().count();
-    }
-
-    KNotificationAction *actionAt(qsizetype index)
-    {
-        return actions().at(index);
-    }
-
-    QQmlListProperty<KNotificationAction> actionsProperty() const
-    {
-        return m_actionsProperty;
-    }
-
-    static qsizetype actionsCount(QQmlListProperty<KNotificationAction> *list)
-    {
-        return static_cast<NotificationWrapper *>(list->object)->actionCount();
-    }
-
-    static void appendAction(QQmlListProperty<KNotificationAction> *list, KNotificationAction *value)
-    {
-        auto notification = static_cast<NotificationWrapper *>(list->object);
-        auto actions = notification->actions();
-        actions << value;
-        notification->setActionsQml(actions);
-    }
-
-    static KNotificationAction *actionAt(QQmlListProperty<KNotificationAction> *list, qsizetype index)
-    {
-        return static_cast<NotificationWrapper *>(list->object)->actionAt(index);
-    }
-
-    static void clearActions(QQmlListProperty<KNotificationAction> *list)
-    {
-        auto notification = static_cast<KNotification *>(list->object);
-        notification->clearActions();
-    }
-
-private:
-    QQmlListProperty<KNotificationAction> m_actionsProperty;
-};
-
-class NotificationPermissionWrapper
-{
-    Q_GADGET
-public:
-    Q_INVOKABLE bool checkPermission()
-    {
-        return KNotificationPermission::checkPermission() == Qt::PermissionStatus::Granted;
-    }
-
-    Q_INVOKABLE void requestPermission(const QJSValue &callback)
-    {
-        KNotificationPermission::requestPermission(m_engine, [&callback](Qt::PermissionStatus status) {
-            callback.call({status == Qt::PermissionStatus::Granted});
-        });
-    }
-
-    QQmlEngine *m_engine = nullptr;
-};
-
-void KNotificationQmlPlugin::registerTypes(const char *uri)
-{
-    Q_ASSERT(QLatin1String(uri) == QLatin1String("org.kde.notification"));
-    qmlRegisterType<NotificationWrapper>(uri, 1, 0, "Notification");
-    qmlRegisterType<KNotificationAction>(uri, 1, 0, "NotificationAction");
-    qmlRegisterUncreatableType<KNotificationReplyAction>(uri, 1, 0, "NotificationReplyAction", {});
-    qmlRegisterSingletonType("org.kde.notification", 1, 0, "NotificationPermission", [](QQmlEngine *engine, QJSEngine *) -> QJSValue {
-        return engine->toScriptValue(NotificationPermissionWrapper{engine});
-    });
+    m_actionsProperty = QQmlListProperty<KNotificationAction>(this,
+                                                              nullptr,
+                                                              &NotificationWrapper::appendAction,
+                                                              &NotificationWrapper::actionsCount,
+                                                              &NotificationWrapper::actionAt,
+                                                              &NotificationWrapper::clearActions);
 }
 
-#include "knotificationqmlplugin.moc"
+KNotificationReplyAction *NotificationWrapper::replyActionFactory()
+{
+    if (!replyAction()) {
+        setReplyAction(std::make_unique<KNotificationReplyAction>(QString()));
+    }
+    return replyAction();
+}
+
+int NotificationWrapper::actionCount() const
+{
+    return actions().count();
+}
+
+KNotificationAction *NotificationWrapper::actionAt(qsizetype index)
+{
+    return actions().at(index);
+}
+
+QQmlListProperty<KNotificationAction> NotificationWrapper::actionsProperty() const
+{
+    return m_actionsProperty;
+}
+
+qsizetype NotificationWrapper::actionsCount(QQmlListProperty<KNotificationAction> *list)
+{
+    return static_cast<NotificationWrapper *>(list->object)->actionCount();
+}
+
+void NotificationWrapper::appendAction(QQmlListProperty<KNotificationAction> *list, KNotificationAction *value)
+{
+    auto notification = static_cast<NotificationWrapper *>(list->object);
+    auto actions = notification->actions();
+    actions << value;
+    notification->setActionsQml(actions);
+}
+
+KNotificationAction *NotificationWrapper::actionAt(QQmlListProperty<KNotificationAction> *list, qsizetype index)
+{
+    return static_cast<NotificationWrapper *>(list->object)->actionAt(index);
+}
+
+void NotificationWrapper::clearActions(QQmlListProperty<KNotificationAction> *list)
+{
+    auto notification = static_cast<KNotification *>(list->object);
+    notification->clearActions();
+}
+
 #include "moc_knotificationqmlplugin.cpp"
--- src/qml/knotificationqmlplugin.h
+++ src/qml/knotificationqmlplugin.h
@@ -8,14 +8,71 @@
 #define KNOTIFICATIONQMLPLUGIN_H
 
 #include <QQmlEngine>
-#include <QQmlExtensionPlugin>
 
-class KNotificationQmlPlugin : public QQmlExtensionPlugin
+#include <KNotification>
+#include <KNotificationPermission>
+#include <KNotificationReplyAction>
+
+struct NotificationActionForeign {
+    Q_GADGET
+    QML_NAMED_ELEMENT(NotificationAction)
+    QML_FOREIGN(KNotificationAction);
+};
+
+struct NotificationReplyActionForeign {
+    Q_GADGET
+    QML_NAMED_ELEMENT(NotificationReplyAction)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(KNotificationReplyAction);
+};
+
+class NotificationWrapper : public KNotification
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+    QML_NAMED_ELEMENT(Notification)
+    Q_PROPERTY(KNotificationReplyAction *replyAction READ replyActionFactory CONSTANT)
+    Q_PROPERTY(QQmlListProperty<KNotificationAction> actions READ actionsProperty NOTIFY actionsChanged)
+    Q_PROPERTY(KNotificationAction *defaultAction READ defaultAction WRITE setDefaultActionQml NOTIFY defaultActionChanged)
 public:
-    void registerTypes(const char *uri) override;
+    explicit NotificationWrapper(QObject *parent = nullptr);
+
+    KNotificationReplyAction *replyActionFactory();
+
+    int actionCount() const;
+
+    KNotificationAction *actionAt(qsizetype index);
+
+    QQmlListProperty<KNotificationAction> actionsProperty() const;
+
+    static qsizetype actionsCount(QQmlListProperty<KNotificationAction> *list);
+
+    static void appendAction(QQmlListProperty<KNotificationAction> *list, KNotificationAction *value);
+
+    static KNotificationAction *actionAt(QQmlListProperty<KNotificationAction> *list, qsizetype index);
+
+    static void clearActions(QQmlListProperty<KNotificationAction> *list);
+
+private:
+    QQmlListProperty<KNotificationAction> m_actionsProperty;
+};
+
+class NotificationPermissionWrapper : public QObject
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(NotificationPermission)
+    QML_SINGLETON
+public:
+    Q_INVOKABLE bool checkPermission()
+    {
+        return KNotificationPermission::checkPermission() == Qt::PermissionStatus::Granted;
+    }
+
+    Q_INVOKABLE void requestPermission(const QJSValue &callback)
+    {
+        KNotificationPermission::requestPermission(this, [&callback](Qt::PermissionStatus status) {
+            callback.call({status == Qt::PermissionStatus::Granted});
+        });
+    }
 };
 
 #endif // KNOTIFICATIONQMLPLUGIN_H
